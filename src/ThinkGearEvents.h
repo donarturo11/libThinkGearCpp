ba1081923c8dab0731a96a38a5eface76e2ebe94
Artur Wrona <arturwrona91@gmail.com>
2023-06-12 12:58:00 +0200

    Fix bind member functions

--- src/ThinkGearEvents.h
+++ src/ThinkGearEvents.h
@@ -16,28 +16,28 @@ public:
     
     template<class ListenerClass>
     void connectListener(ListenerClass *listener) {
-        TGEventConnect(onRaw, listener, ListenerClass::onThinkGearRaw);
-        TGEventConnect(onPoorSignal, listener, ListenerClass::onThinkGearPoorSignal);
-        TGEventConnect(onBlinkStrength, listener, ListenerClass::onThinkGearBlinkStrength);
-        TGEventConnect(onAttention, listener, ListenerClass::onThinkGearAttention);
-        TGEventConnect(onMeditation, listener, ListenerClass::onThinkGearMeditation);
-        TGEventConnect(onEeg, listener, ListenerClass::onThinkGearEeg);
-        TGEventConnect(onConnecting, listener, ListenerClass::onThinkGearConnecting);
-        TGEventConnect(onReady, listener, ListenerClass::onThinkGearReady);
-        TGEventConnect(onError, listener, ListenerClass::onThinkGearError);
+        TGEventConnect(&onRaw, listener, &ListenerClass::onThinkGearRaw);
+        TGEventConnect(&onPoorSignal, listener, &ListenerClass::onThinkGearPoorSignal);
+        TGEventConnect(&onBlinkStrength, listener, &ListenerClass::onThinkGearBlinkStrength);
+        TGEventConnect(&onAttention, listener, &ListenerClass::onThinkGearAttention);
+        TGEventConnect(&onMeditation, listener, &ListenerClass::onThinkGearMeditation);
+        TGEventConnect(&onEeg, listener, &ListenerClass::onThinkGearEeg);
+        TGEventConnect(&onConnecting, listener, &ListenerClass::onThinkGearConnecting);
+        TGEventConnect(&onReady, listener, &ListenerClass::onThinkGearReady);
+        TGEventConnect(&onError, listener, &ListenerClass::onThinkGearError);
     }
     
     template<class ListenerClass>
     void disconnectListener(ListenerClass *listener) {
-        TGEventDisconnect(onRaw, listener, ListenerClass::onThinkGearRaw);
-        TGEventDisconnect(onPoorSignal, listener, ListenerClass::onThinkGearPoorSignal);
-        TGEventDisconnect(onBlinkStrength, listener, ListenerClass::onThinkGearBlinkStrength);
-        TGEventDisconnect(onAttention, listener, ListenerClass::onThinkGearAttention);
-        TGEventDisconnect(onMeditation, listener, ListenerClass::onThinkGearMeditation);
-        TGEventDisconnect(onEeg, listener, ListenerClass::onThinkGearEeg);
-        TGEventDisconnect(onConnecting, listener, ListenerClass::onThinkGearConnecting);
-        TGEventDisconnect(onReady, listener, ListenerClass::onThinkGearReady);
-        TGEventDisconnect(onError, listener, ListenerClass::onThinkGearError);
+        TGEventDisconnect(&onRaw, listener, &ListenerClass::onThinkGearRaw);
+        TGEventDisconnect(&onPoorSignal, listener, &ListenerClass::onThinkGearPoorSignal);
+        TGEventDisconnect(&onBlinkStrength, listener, &ListenerClass::onThinkGearBlinkStrength);
+        TGEventDisconnect(&onAttention, listener, &ListenerClass::onThinkGearAttention);
+        TGEventDisconnect(&onMeditation, listener, &ListenerClass::onThinkGearMeditation);
+        TGEventDisconnect(&onEeg, listener, &ListenerClass::onThinkGearEeg);
+        TGEventDisconnect(&onConnecting, listener, &ListenerClass::onThinkGearConnecting);
+        TGEventDisconnect(&onReady, listener, &ListenerClass::onThinkGearReady);
+        TGEventDisconnect(&onError, listener, &ListenerClass::onThinkGearError);
     }
     
     TGEvent<short> onRaw;
